528892f74f9d622e0b799323cf54caed9f5d60d4
Nami Ali <1711206138@qq.com>
2019-12-14 13:59:10 +0500

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+     <b-nav>
+    <b-nav-item active>Fancy Quiz App</b-nav-item>
+    <b-nav-item>Questions {{num_correct}}/{{num_total}}</b-nav-item>
+  </b-nav>
+   </div>
+</template>
+
+
+<script>
+ 
+  export default {
+    props: [
+      'num_correct',
+      'num_total'
+    ]
+  }
+
+</script>
